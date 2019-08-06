33ca13ec66b1cd74bcd7aa414f66fed866bc943e
srwatlanta <shawnrwheeler@gmail.com>
2019-08-06 18:14:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(oogleyboogley => renderBooks(oogleyboogley));
 }
 
 function renderBooks(json) {
